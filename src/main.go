19fdab141157efc5a45e886da34907f933265ff7
Yuta.Imamura <atuuyarumami0704@yahoo.co.jp>
2026-01-11 17:29:29 +0900

    ✨ 基本的なwebサーバ

--- src/main.go
+++ src/main.go
@@ -1,8 +1,47 @@
 package main
 
-import "fmt"
+import (
+	//"fmt"
+	"log"
+	"text/template"
+	"net/http"
+)
+
+// トップページハンドラ (/)
+func topPageHandler(w http.ResponseWriter, r *http.Request) {
+	filename := "templates/index.html"
+	tmpl, err := template.ParseFiles(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+// registerページハンドラ (/register)
+func registerPageHandler(w http.ResponseWriter, r *http.Request) {
+	filename := "templates/register.html"
+	tmpl, err := template.ParseFiles(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./templates/css"))))
+	http.HandleFunc("/", topPageHandler)
+	http.HandleFunc("/register", registerPageHandler)
+	
+	http.ListenAndServe(":8080", nil)
 }
 
